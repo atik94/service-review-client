f0d778c635cc9c8c9cd5bcc69dd3475295b849b9
atik94 <atikurrahmanatik94@gmail.com>
2022-11-08 21:40:38 +0600

    service section design

---
+++ src/Pages/Home/Services/ServiceCard.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const ServiceCard = ({ service }) => {
+  const { img, title, price } = service;
+  return (
+    <div className="card card-side bg-base-100 shadow-xl">
+      <figure>
+        <img src={img} alt="Movie" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{title}</h2>
+        <p className="text-2xl font-bold text-orange-600">Price: $ {price}</p>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary">Watch</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ServiceCard;
---
+++ src/Pages/Home/Services/Services.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+import ServiceCard from "./ServiceCard";
+
+const Services = () => {
+  const [services, setServices] = useState([]);
+  useEffect(() => {
+    fetch("services.json")
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, []);
+  return (
+    <div>
+      <div className="text-center mb-2">
+        <h2 className="text-5xl font-bold text-orange-600">Services</h2>
+        <h2 className="text-3xl font-semibold ">Our service area</h2>
+      </div>
+      <div className="grid gap-6 grid-cols-1 md:grid-cols-1 lg:grid-cols-1">
+        {services.map((service) => (
+          <ServiceCard key={service._id} service={service}></ServiceCard>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Services;
