68604a540cc0f5c2bec58a9677bfae23b298486a
atik94 <atikurrahmanatik94@gmail.com>
2022-11-09 02:28:32 +0600

    login page design

---
+++ src/Pages/Login/Login.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  const handleLogin = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <div className="hero min-h-screen bg-base-200 my-20 ">
+      <div className="hero-content ">
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 py-20">
+          <h1 className="text-5xl font-bold text-center">Login now!</h1>
+          <form onSubmit={handleLogin} className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input type="text" name="email" placeholder="email" className="input input-bordered" />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input type="password" name="password" placeholder="password" className="input input-bordered" />
+              <label className="label">
+                <a href="#" className="label-text-alt link link-hover">
+                  Forgot password?
+                </a>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <input type="submit" value="Login" className="btn btn-info" />
+            </div>
+          </form>
+          <p className="text-center">
+            New to Landy service ?{" "}
+            <Link className="text-2xl font-bold text-orange-600" to="/signup">
+              Sign Up
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
