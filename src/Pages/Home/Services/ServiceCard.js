68dc3919841298568d17650d936c6acc51f4b084
atik94 <atikurrahmanatik94@gmail.com>
2022-11-09 18:54:42 +0600

    review send to database

--- src/Pages/Home/Services/ServiceCard.js
+++ src/Pages/Home/Services/ServiceCard.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const ServiceCard = ({ service }) => {
-  const { _id, img, title, price } = service;
+  const { _id, img, title, price, description, rating } = service;
   return (
     <div className="card card-side bg-base-100 shadow-xl">
       <figure>
@@ -10,6 +10,8 @@ const ServiceCard = ({ service }) => {
       </figure>
       <div className="card-body">
         <h2 className="card-title">{title}</h2>
+        <p>{description}</p>
+        <h3>{rating}</h3>
         <p className="text-2xl font-bold text-orange-600">Price: $ {price}</p>
         <div className="card-actions justify-end">
           <Link to={`/viewDetails/${_id}`}>
--- src/Pages/Home/ViewDetails/ViewDetails.js
+++ src/Pages/Home/ViewDetails/ViewDetails.js
@@ -1,11 +1,82 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useLoaderData } from "react-router-dom";
+import { AuthContext } from "../../../contexts/AuthProvider/AuthProvider";
 
 const ViewDetails = () => {
-  const { title } = useLoaderData();
+  const { _id, title, img, price, description, rating } = useLoaderData();
+  const { user } = useContext(AuthContext);
+  const handleReview = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = `${form.firstName.value} ${form.lastName.value}`;
+    const email = user?.email || "Unregistered";
+    const image = form.image.value;
+    const message = form.message.value;
+    const review = {
+      service: _id,
+      customer: name,
+      email,
+      message,
+      image,
+    };
+    fetch("http://localhost:5000/reviews", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(review),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.acknowledged) {
+          alert("Review submit successfully");
+          form.reset();
+        }
+      })
+      .catch((err) => console.log(err));
+  };
   return (
     <div>
-      <h2>{title}</h2>
+      <div>
+        <form onSubmit={handleReview}>
+          <div className="card card-side bg-base-100 shadow-xl">
+            <figure>
+              <img src={img} style={{ minWidth: "800px" }} alt="Movie" />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">{title}</h2>
+              <p>{description}</p>
+              <h3>Rating: {rating}</h3>
+              <p className="text-2xl font-bold text-orange-600">Price: $ {price}</p>
+              <div className="card-actions justify-end"></div>
+            </div>
+          </div>
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mt-5">
+            <input name="firstName" type="text" placeholder="First name" className="input input-bordered w-full" />
+            <input name="lastName" type="text" placeholder="Last name" className="input input-bordered w-full" />
+            <input name="image" type="text" placeholder="your img url" className="input input-bordered w-full" />
+            {/* <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" /> */}
+            <input
+              name="email"
+              type="text"
+              placeholder="your email"
+              defaultValue={user?.email}
+              className="input input-bordered w-full"
+              readOnly
+            />
+          </div>
+          <textarea
+            name="message"
+            className="textarea textarea-bordered h-24 w-full"
+            placeholder="Write your review here:"
+          ></textarea>
+          <input className="btn btn-info" type="submit" value="submit" />
+        </form>
+      </div>
+      <div>
+        <h2>review section</h2>
+      </div>
     </div>
   );
 };
